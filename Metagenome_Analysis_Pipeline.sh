ec499f8028914c23cfbcdbbbec19e1b41c1d3527
KimBarnes <41737754+KimBarnes@users.noreply.github.com>
2018-07-26 12:50:36 +0100

    Update Metagenome_Analysis_Pipeline.sh

--- Metagenome_Analysis_Pipeline.sh
+++ Metagenome_Analysis_Pipeline.sh
@@ -1,12 +1,12 @@
-directory="Anna_MetaBat_bins"
-#python $directory/Cluster_Analysis/FileSize.py
+directory=""
+python $directory/Cluster_Analysis/FileSize_Filter.py
 checkm lineage_wf -t 64 -x fasta $directory/ANALYSIS/ $directory/ANALYSIS/CheckM
 python $directory/Cluster_Analysis/CheckM_Parser1.py
-#python $directory/Cluster_Analysis/Sequence_Analysis.py
-#for D in $directory/ANALYSIS/Sequence_Composition/*;
-#do checkm lineage_wf -t 64 -x fasta $D/ $D/CheckM
-#done
-#python $directory/Cluster_Analysis/CheckM_Parser2.py
+python $directory/Cluster_Analysis/Multi-Contig_Contamination_Removal.py
+for D in $directory/ANALYSIS/Sequence_Composition/*;
+do checkm lineage_wf -t 64 -x fasta $D/ $D/CheckM
+done
+python $directory/Cluster_Analysis/CheckM_Parser2.py
 setup_prokka
 cd ~/$directory/ANALYSIS/Genome_Annotation/Bacteria
 for file in *.fasta;
@@ -17,4 +17,4 @@ for file in *.fasta;
 do prokka -metagenome "$file" -outdir Prokka/$file -locustag $file -kingdom Archaea -centre X -compliant
 done
 cd ~
-python $directory/Cluster_Analysis/Metabolic_Pathways_Linux.py
+python $directory/Cluster_Analysis/Metabolic_Pathway_Prediction.py
